e427b4d2b3107c835e7e3b363a4bb0acaa5dc439
Graham Kennery <graham@kennery.com>
2013-08-07 20:41:36 -0400

    Bug fixes

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ RedisCallback.prototype._unsubscribe = function(event, functino) {
 
 
 RedisCallback.prototype.exec = function(event, cb) {
-	var params = [].slice.call(parameters);
+	var params = [].slice.call(arguments);
 	event = params.shift();
 	cb = params.pop();
 
